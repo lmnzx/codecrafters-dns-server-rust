f772184d078c1c5830f546200407ca295e8b8475
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2024-01-01 19:13:41 +0530

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
 use std::net::UdpSocket;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    // let mut buf = [0; 512];
-    //
-    // loop {
-    //     match udp_socket.recv_from(&mut buf) {
-    //         Ok((size, source)) => {
-    //             let _received_data = String::from_utf8_lossy(&buf[0..size]);
-    //             println!("Received {} bytes from {}", size, source);
-    //             let response = [];
-    //             udp_socket
-    //                 .send_to(&response, source)
-    //                 .expect("Failed to send response");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error receiving data: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
-
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
 
